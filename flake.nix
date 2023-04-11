29092f7d84ae0813b0a915de3e75050b9b77c229
Xe Iaso <xe@tailscale.com>
2023-04-11 14:46:55 +0000

    first commit
    Signed-off-by: Xe Iaso <xe@tailscale.com>

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Flake utils demo";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [ dive tree go_1_20 go-tools gotools gopls ];
+        };
+      }
+    );
+}
